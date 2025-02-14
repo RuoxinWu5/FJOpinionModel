4f3ed285ca1c0b65d126f354e59b6a77051eb536
RuoxinWu5 <wuruoxin2022@163.com>
2025-02-14 17:20:28 +0800

    PC-own

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PC-school!!')
+    print_hi('PC-school & PC-own!!')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
