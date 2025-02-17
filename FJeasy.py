6f19376d606344ba01686b8daedec3f7122ab887
RuoxinWu5 <wuruoxin2022@163.com>
2025-02-17 20:07:05 +0800

    Check to call FJModel

--- FJeasy.py
+++ FJeasy.py
@@ -1,10 +1,13 @@
-# Friedkin-Johnsen模型的一个简单版本，用于模拟一个小型有向图中节点观点的演变过程。
-# 主要步骤包括：
-# 1.定义图结构（邻接矩阵）
-# 2.生成内部观点
-# 3.初始化表达观点
-# 4.通过迭代更新表达观点，直到达到平衡状态或达到最大迭代次数。
+"""
+Friedkin-Johnsen模型的一个简单版本，用于模拟一个小型有向图中节点观点的演变过程。
+主要步骤包括：
+1.定义图结构（邻接矩阵）
+2.生成内部观点
+3.初始化表达观点
+4.通过迭代更新表达观点，直到达到平衡状态或达到最大迭代次数。
+"""
 import numpy as np
+from FJmodel import FJModel
 
 # Step1: 手动定义小型网络邻接矩阵（有向、类型为浮点数） 0->1->2
 A = np.array([
@@ -39,3 +42,10 @@ for step in range(max_iter):
     # 如果有收敛条件，将迭代次数max_iter设置稍大一点
     # 在此处检查是否收敛，若收敛就break
     # print(f"在第 {step + 1} 步收敛")
+
+"""
+此处尝试调用 FJModel 类及其 iterate() 方法
+"""
+model = FJModel(A, s)
+z_history = model.iterate(max_iter)
+print("z_history", z_history)
